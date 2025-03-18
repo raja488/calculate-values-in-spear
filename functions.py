81c38041fc9c6f6fd51bb596c89127614816d090
umar <umarqadoos93@gmail.com>
2025-03-18 18:23:33 +0000

    functiond

---
+++ functions.py
@@ -0,0 +1,14 @@
+def my_name():
+    print("i am printing this with the help of functions ")
+
+
+my_name()
+
+def add():
+    
+    number1=int(input("enter a number"))
+    number2=int(input("enter a number"))
+    final_number=number1+number2
+    print(final_number)
+
+add()
---
+++ main.py
@@ -0,0 +1,12 @@
+import math as m
+
+#calculate the square root of the given number
+
+number=int(input("enter a value "))
+
+print(m.sqrt(number))
+
+#calculate the factorial of the given number 
+print(m.factorial(number))
+
+
